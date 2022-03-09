cacec0a981dc834481cb6a2debcbbd5dc428009d
NitsuG <NitsuGGG@gmail.com>
2022-03-09 20:52:20 +0100

    Expérience + WIP Mes compétences

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 let express = require('express')
+const jauge = require('./public/Script/jauge')
 let app = express()
+
+app.use(express.urlencoded({ extended: false})) //Utilisé pour receptionné les donnée d'un formulaire
+app.use(express.json())
 
 app.use('/assets', express.static('public'))
 app.set('view engine', 'ejs')
 
 
 app.get("/", (request, response) => {
+
     response.render("./Layouts/home-view")
 })
 
